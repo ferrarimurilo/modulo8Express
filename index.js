c59298573a680fb64f5698f066bd808a16cd7800
Murilo <ferrarimurilo10@gmail.com>
2024-08-13 20:17:42 -0300

    Configurações básicas express

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+
+const porta = 3000;
+
+app.listen(porta, () => {
+    console.log(`O servidor está ativo pela porta ${porta}`);
+    console.log(`O servidor está ativo pela porta ${porta}`);
+
+});
+
+
