473673d2ea97577a5ba5af3c21cfb766ce4a3640
Murilo <ferrarimurilo10@gmail.com>
2024-08-13 21:16:35 -0300

    primeiro exercicio com parametros

--- index.js
+++ index.js
@@ -4,8 +4,16 @@ const app = express();
 
 const porta = 3000;
 
+app.get('/segundaRota',(req,res) => {
+    res.send(`Saudações Terraquio!!`);
+})
+
+app.get('/cadastro/:nome/:idade/:curso',(req,res) => {
+    req.params;
+    res.send(`O nome digitado foi ${req.params.nome} a idade é ${req.params.idade} e o curso é ${req.params.curso}`);
+})
+
 app.listen(porta, () => {
-    console.log(`O servidor está ativo pela porta ${porta}`);
     console.log(`O servidor está ativo pela porta ${porta}`);
 
 });
